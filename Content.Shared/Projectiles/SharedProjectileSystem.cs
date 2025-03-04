fdfe92beb7de29735b02b8dc264676a1677ab86b
Whatstone <whatston3@gmail.com>
2025-03-04 15:31:34 -0500

    SharedProjectileSystem: fix shooter/weapon null

--- Content.Shared/Projectiles/SharedProjectileSystem.cs
+++ Content.Shared/Projectiles/SharedProjectileSystem.cs
@@ -88,7 +88,7 @@ public abstract partial class SharedProjectileSystem : EntitySystem
         // Raise a specific event for projectiles.
         if (TryComp(uid, out ProjectileComponent? projectile))
         {
-            var ev = new ProjectileEmbedEvent(projectile.Shooter!.Value, projectile.Weapon!.Value, args.Target);
+            var ev = new ProjectileEmbedEvent(projectile.Shooter, projectile.Weapon ?? EntityUid.Invalid, args.Target); // Frontier: handle nullability on Shooter, Weapon
             RaiseLocalEvent(uid, ref ev);
         }
     }
